aac016c638863684b9b9e5a4c6870f83a2397672
Steven Meads <sbmeads@gmail.com>
2016-07-18 14:27:51 -0700

    Checkpoint 39 PostUser: completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,16 @@
 
 require 'random_data'
 
+# Create Users
+5.times do
+   User.create!(
+   name: RandomData.random_name,
+   email: RandomData.random_email,
+   password: RandomData.random_sentence
+   )
+ end
+ users = User.all
+
 # Create Topics
 15.times do
   Topic.create!(
@@ -15,10 +25,12 @@ require 'random_data'
   description: RandomData.random_paragraph
   )
 end
+topics = Topic.all
 
 # Create Posts
 50.times do
   Post.create!(
+    user: users.sample,
     topic: topics.sample,
     title: RandomData.random_sentence,
     body: RandomData.random_paragraph
@@ -34,7 +46,14 @@ posts = Post.all
   )
 end
 
+user = User.first
+user.update_attributes!(
+  email: 'sbmeads@gmail.com',
+  password: 'helloworld'
+ )
+
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
