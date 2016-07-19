58f869e3d4837783997a8463a4cc7144e32518fa
Steven Meads <sbmeads@gmail.com>
2016-07-19 10:38:54 -0700

    Checkpoint 42 Assignment: partial

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,32 +4,36 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     comment = @post.comments.new(comment_params)
+    @topic = Topic.find(params[:post_id])
+    comment = @topic.comments.new(comment_params)
     comment.user = current_user
 
     if comment.save
       flash[:notice] = "Comment saved successfully."
-      redirect_to [@post.topic, @post]
+      redirect_to [@post.topic, @post] # need to redirect_to topic_post_path?
     else
       flash[:alert] = "Comment failed to save."
-      redirect_to [@post.topic, @post]
+      redirect_to [@post.topic, @post] # need to redirect_to topic_post_path?
     end
   end
 
   def destroy
      @post = Post.find(params[:post_id])
      comment = @post.comments.find(params[:id])
+     @topic = Topic.find(params[:post_id])
+     comment = @topic.comments.find(params[:id])
 
      if comment.destroy
        flash[:notice] = "Comment was deleted successfully."
-       redirect_to [@post.topic, @post]
+       redirect_to [@post.topic, @post] # need to redirect_to topic_post_path?
      else
        flash[:alert] = "Comment couldn't be deleted. Try again."
-       redirect_to [@post.topic, @post]
+       redirect_to [@post.topic, @post] # need to redirect_to topic_post_path?
      end
    end
 
   private
-  
+
   def comment_params
     params.require(:comment).permit(:body)
   end
