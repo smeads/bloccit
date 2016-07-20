0be6be0a6fb6c3021a0e595b89bb5f1d82992cf3
Steven Meads <sbmeads@gmail.com>
2016-07-20 15:00:23 -0700

    Checkpoint 47 Ajax: partial

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -17,19 +17,21 @@ class CommentsController < ApplicationController
 
   def destroy
      @post = Post.find(params[:post_id])
-     comment = @post.comments.find(params[:id])
+     @comment = @post.comments.find(params[:id])
 
-     if comment.destroy
+     if @comment.destroy
        flash[:notice] = "Comment was deleted successfully."
-       redirect_to [@post.topic, @post]
      else
        flash[:alert] = "Comment couldn't be deleted. Try again."
-       redirect_to [@post.topic, @post]
+     end
+     respond_to do |format|
+       format.html {redirect_to [@post.topic, @post]}
+       format.js
      end
    end
 
   private
-  
+
   def comment_params
     params.require(:comment).permit(:body)
   end
@@ -41,5 +43,4 @@ class CommentsController < ApplicationController
        redirect_to [comment.post.topic, comment.post]
      end
    end
-
 end
