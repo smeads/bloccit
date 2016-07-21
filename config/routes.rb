274449bc4bd571a86cde4830ddd19824bb787eac
Steven Meads <sbmeads@gmail.com>
2016-07-21 14:33:58 -0700

    Checkpoint 49 Assignment: completed

--- config/routes.rb
+++ config/routes.rb
@@ -25,8 +25,9 @@ Rails.application.routes.draw do
   namespace :api do
      namespace :v1 do
        resources :users, only: [:index, :show, :create, :update]
-       resources :topics, except: [:edit, :new]
-       resources :posts, except: [:edit, :new]
+       resources :topics, except: [:edit, :new] do
+        resources :posts, except: [:edit, :new]
+       end
      end
    end
 
