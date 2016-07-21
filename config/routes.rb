496379bb2e0e5b3d38bc31e64cb9ba1fd7a1f89c
Steven Meads <sbmeads@gmail.com>
2016-07-20 18:14:20 -0700

    Checkpoint 48 API: completed

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,14 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
+  namespace :api do
+     namespace :v1 do
+       resources :users, only: [:index, :show]
+       resources :topics, only: [:index, :show]
+     end
+   end
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
